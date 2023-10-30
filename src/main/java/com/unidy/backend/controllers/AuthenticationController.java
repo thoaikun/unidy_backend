efef17eb48e13f0f2cfeb4b55e678890f610b21b
Huy-Thai-3105 <80185943+Huy-Thai-3105@users.noreply.github.com>
2023-10-30 09:39:35 +0700

    add feature submit OTP

--- src/main/java/com/unidy/backend/controllers/AuthenticationController.java
+++ src/main/java/com/unidy/backend/controllers/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.unidy.backend.controllers;
 
 import com.unidy.backend.domains.dto.requests.AuthenticationRequest;
+import com.unidy.backend.domains.dto.requests.OTPRequest;
 import com.unidy.backend.domains.dto.requests.ResetPasswordRequest;
 import com.unidy.backend.services.servicesInterface.ResetPassword;
 import com.unidy.backend.services.servicesIplm.AuthenticationServiceIplm;
@@ -52,6 +53,21 @@ public class AuthenticationController {
       }
       else{
         return ResponseEntity.ok().body("Send OTP success");
+      }
+    } catch (Exception e){
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Hệ thống xảy ra lỗi");
+    }
+  }
+
+  @PostMapping("/submit-OPT")
+  public ResponseEntity<?> submitOTP(@RequestBody OTPRequest OTP){
+
+    try {
+      if (resetPassword.submitOTP(OTP)){
+        return ResponseEntity.ok().body("Submit OTP success");
+      }
+      else{
+        return ResponseEntity.badRequest().body("Submit OTP fail");
       }
     } catch (Exception e){
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Hệ thống xảy ra lỗi");
--- src/main/java/com/unidy/backend/repositories/OtpRepository.java
+++ src/main/java/com/unidy/backend/repositories/OtpRepository.java
@@ -1,11 +1,19 @@
 package com.unidy.backend.repositories;
 
 import com.unidy.backend.domains.entity.Otp;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.Optional;
 
+@Transactional
 public interface OtpRepository extends JpaRepository<Otp, Integer> {
-    public void deleteByUserId(Integer userId);
-    public Optional<Otp> findByUserId(Integer userId);
+     @Modifying
+     void deleteByUserId(Integer userId);
+
+     @Modifying
+     void deleteByOtpCode(String otpCode);
+     Optional<Otp> findByUserId(Integer userId);
+     Optional<Otp> findByOtpCode(String OTP);
 }
