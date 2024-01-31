3552672f58c60896e79151edcfdd1ff1a90b5c72
Huy-Thai-3105 <80185943+Huy-Thai-3105@users.noreply.github.com>
2024-01-31 22:23:51 +0700

    add check chosed favorite activity (api login)

--- src/main/java/com/unidy/backend/domains/dto/responses/AuthenticationResponse.java
+++ src/main/java/com/unidy/backend/domains/dto/responses/AuthenticationResponse.java
@@ -16,4 +16,6 @@ public class AuthenticationResponse {
   private String accessToken;
   @JsonProperty("refresh_token")
   private String refreshToken;
+  @JsonProperty("isChosedFavorite")
+  private Boolean isChosedFavorite;
 }
