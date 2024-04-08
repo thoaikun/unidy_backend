d9d49f422b8035f122fda4bbb488f028c0b8eb16
Huy-Thai-3105 <80185943+Huy-Thai-3105@users.noreply.github.com>
2024-04-08 08:39:56 +0700

    add preAuthorize for admin api

--- src/main/java/com/unidy/backend/controllers/AdminController.java
+++ src/main/java/com/unidy/backend/controllers/AdminController.java
@@ -27,7 +27,6 @@ public class AdminController {
         return adminService.register(request);
     }
 
-
     @PostMapping("/authenticate/login")
     public ResponseEntity<?> authenticate(
             @RequestBody AuthenticationRequest request
@@ -35,21 +34,25 @@ public class AdminController {
         return adminService.authenticate(request);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/runOrStopJob")
     public ResponseEntity<?> runOrStopScheduleJob(@RequestParam("jobId") int jobId){
         return adminService.runOrStopJob(jobId);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/posts/{postId}/block/{block}")
     public ResponseEntity<?> blockOrUnblockPost(@PathVariable String postId,@PathVariable String block){
         return adminService.blockOrUnblockPost(postId,block);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/posts/{postId}")
     public ResponseEntity<?> deletePost(@PathVariable String postId){
         return adminService.deletePost(postId);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/posts/date")
     public ResponseEntity<?> getPostByDate(
             @RequestParam("fromDate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") Date fromDate,
@@ -60,22 +63,26 @@ public class AdminController {
         return adminService.getPostByDate(fromDate, toDate, skip, limit);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/organization/{organizationId}/approve")
     public ResponseEntity<?> approveOrganization(@PathVariable int organizationId){
         return adminService.approveOrganization(organizationId);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/users/{userId}/block")
     public ResponseEntity<?> blockOrUnblockUser(@PathVariable int userId){
         return adminService.blockOrUnblockUser(userId);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/campaigns/{status}")
     public ResponseEntity<?> getCampaignByStatus(@PathVariable CampaignStatus status, @RequestParam("skip") int skip,
                                                  @RequestParam("limit") int limit ){
         return adminService.getCampaignByStatus(status,skip,limit);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/campaigns/date")
     public ResponseEntity<?> getCampaignPostByDate(
             @RequestParam("fromDate") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") Date fromDate,
@@ -87,6 +94,7 @@ public class AdminController {
     }
 
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/settlements/{settlementId}/confirm")
     public ResponseEntity<?> confirmSettlements(@PathVariable int settlementId, Principal userConnected){
         return adminService.confirmSettlements(settlementId,userConnected);
